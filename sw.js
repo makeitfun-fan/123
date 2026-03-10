278fa6ceee4378978268ece7d5ed7eb47005970f
makeitfun-fan <1071890138s@gmail.com>
2026-03-10 14:39:34 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,23 @@
-const CACHE = 'couples-app-v1';
+const CACHE = 'couples-app-v10';
 const ASSETS = [
   '/couple-app/',
   '/couple-app/index.html'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(keys.map(k => caches.delete(k)))
     )
   );
   self.clients.claim();
 });
 
+// 永远从网络获取最新内容
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request).catch(() => caches.match(e.request))
